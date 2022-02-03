64beda6a2c376d1eb449a5732b47b982bf5502d3
prabujung <prabujung>
2022-02-03 10:28:33 +0545

    secant update

--- NM/Unit1/2secant.c
+++ NM/Unit1/2secant.c
@@ -13,13 +13,15 @@ int main()
     scanf("%f%f",&x1,&x2);
     printf("Enter tolorable error : ");
     scanf("%f", &e);
+    printf("c \t \t \t b \t \t \t c \t \t\t f(c)");
     while(fabsf((x2-x1)/x1) > e )
     {
-
+        printf("\n%f \t \t%f \t \t",x1,x2);
         x3=(f(x2)*x1-f(x1)*x2)/(f(x2)-f(x1));
         x1=x2;
         x2=x3;
+        printf("%f \t \t%f \t \t",x3,f(x3));
     }
-    printf("The root is %f",x3);
+    printf("\n\nThe root is %f",x3);
     return 0;
 }
